e96b9a5c28230938a5b71751b67b2fa48984fd47
Helen <e.xolodkova@inbox.ru>
2022-04-14 00:45:07 +0300

    Решение задачи №4

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine($"Максимальное из чисел {a} {b} {c}");
+Console.Write("max = ");
+Console.WriteLine(max);
