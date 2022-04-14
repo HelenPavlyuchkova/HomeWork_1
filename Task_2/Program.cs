b4e37cbb0e596328ede34c94a92a0c23deb90f05
Helen <e.xolodkova@inbox.ru>
2022-04-14 16:36:15 +0300

    Решение задачи №2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -13,7 +13,3 @@ else
     Console.Write("max = ");
     Console.WriteLine(max);
 
-
-
-
-//int a = -9, int b = -3; int max = a; if(a > max) max = a; if(b > max) max = b; Console.Write("max = "); Console.WriteLine(max);
