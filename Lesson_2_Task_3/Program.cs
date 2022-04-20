a392f015acc647b6a5ac681765c3a6529c7505ae
Helen <e.xolodkova@inbox.ru>
2022-04-20 20:23:30 +0300

    Добавили задачу №3 (д.з. к семинару 2)

--- Lesson_2_Task_3/Program.cs
+++ Lesson_2_Task_3/Program.cs
@@ -1,23 +1,39 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-System.Console.WriteLine("Введите число: ");
+
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if((number > 99) & (number <= 999))
+
+if(number > 99)
 {
-    System.Console.WriteLine((number % 100) % 10);
-}
-else if((number > 999) & (number <= 9999))
-{
-    System.Console.WriteLine((number % 100) / 10);
-}
-else if((number > 9999) & (number <= 99999))
-{
-    System.Console.WriteLine((number / 100) % 10);
-}
-else if((number > 99999) & (number <= 999999))
-{
-    System.Console.WriteLine((number / 1000) % 10);
-}
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine($"Третья цифра числа: {number}");
+} 
 else
 {
-    System.Console.WriteLine("В указанном числе третьей цифры нет!");
+    Console.WriteLine("Третьей цифры нет");
 }
+
+// if((number > 99) & (number <= 999))
+// {
+//     System.Console.WriteLine((number % 100) % 10);
+// }
+// else if((number > 999) & (number <= 9999))
+// {
+//     System.Console.WriteLine((number % 100) / 10);
+// }
+// else if((number > 9999) & (number <= 99999))
+// {
+//     System.Console.WriteLine((number / 100) % 10);
+// }
+// else if((number > 99999) & (number <= 999999))
+// {
+//     System.Console.WriteLine((number / 1000) % 10);
+// }
+// else
+// {
+//     System.Console.WriteLine("В указанном числе третьей цифры нет!");
+// }
