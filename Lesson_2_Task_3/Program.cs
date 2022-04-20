59a66d1d9c07561e2d1e0b87f8a6bc3640a32de5
Helen <e.xolodkova@inbox.ru>
2022-04-20 21:50:07 +0300

    Добавили задачу №3 (д.з. к семинару 2)

--- Lesson_2_Task_3/Program.cs
+++ Lesson_2_Task_3/Program.cs
@@ -17,23 +17,3 @@ else
     Console.WriteLine("Третьей цифры нет");
 }
 
-// if((number > 99) & (number <= 999))
-// {
-//     System.Console.WriteLine((number % 100) % 10);
-// }
-// else if((number > 999) & (number <= 9999))
-// {
-//     System.Console.WriteLine((number % 100) / 10);
-// }
-// else if((number > 9999) & (number <= 99999))
-// {
-//     System.Console.WriteLine((number / 100) % 10);
-// }
-// else if((number > 99999) & (number <= 999999))
-// {
-//     System.Console.WriteLine((number / 1000) % 10);
-// }
-// else
-// {
-//     System.Console.WriteLine("В указанном числе третьей цифры нет!");
-// }
