7edef5a96bf4477aba0a14eb89e809654d649131
Helen <e.xolodkova@inbox.ru>
2022-04-22 23:04:56 +0300

    Добавили решение задачи №23(таблица кубов)

---
+++ Lesson_3_Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+System.Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+System.Console.Write("Таблица кубов: ");
+
+while(i < N)
+{
+    System.Console.Write($"{Math.Pow(i,3)}, ");
+    i++;
+}
+System.Console.WriteLine($"{Math.Pow(i,3)}.");
