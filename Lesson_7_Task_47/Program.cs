71fc02809e7976ff8cc98c5bb141b91c4198b631
Helen <e.xolodkova@inbox.ru>
2022-05-18 23:08:22 +0300

    Добавили решение задачи 47 к семинару №7

---
+++ Lesson_7_Task_47/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int m = 3;
+int n = 4;
+
+void FillArray(double[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i,j] = Convert.ToDouble(new Random().Next(-100,100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+Console.WriteLine();
+}
+
+// второй вариант
+// Random random = new Random();
+// void FillArray(double[,] mass)
+// {
+//     for (int i = 0; i < mass.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < mass.GetLength(1); j++)
+//         {
+//             mass[i,j] = random.NextDouble() * 10 - 7;
+//         }
+//     }
+// }
+// void PrintArray(double[,] mass)
+// {
+//     for (int i = 0; i < mass.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < mass.GetLength(1); j++)
+//         {
+//             Console.Write($"{Math.Round(mass[i,j],1)} ");
+//         }
+//     Console.WriteLine();
+//     }
+// Console.WriteLine();
+// }
+double[,] array = new double[m,n];
+FillArray(array);
+PrintArray(array);
