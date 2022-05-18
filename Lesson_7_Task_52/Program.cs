bf4c808e657f61bf86c4761236faecf5fb994662
Helen <e.xolodkova@inbox.ru>
2022-05-18 23:09:48 +0300

    Добавили решение задачи 52 к семинару №7

---
+++ Lesson_7_Task_52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int m = 5;
+int n = 4;
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " "); ;
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+
+double[] sum = new double [n];
+for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum[i] += array[j,i];
+        }
+    }
+for (int i = 0; i < sum.Length; i++)
+{
+    Console.Write(sum[i] / m + " ");
+}    
+
+
+    
