088405c0e1d11f93efbbdc3533ed79918607836d
Helen <e.xolodkova@inbox.ru>
2022-05-20 20:05:43 +0300

    Решение задачи №60(семинар 8)

--- Lesson_8_Task_60/Program.cs
+++ Lesson_8_Task_60/Program.cs
@@ -35,6 +35,9 @@ for (int i = 0; i < array.GetLength(0); i++)
     }
     System.Console.WriteLine();
 }
+
+
+// рандомный трехмерный массив(без проверки на повторяемые эл-ты)
 // for (int i = 0; i < array.GetLength(0); i++)
 // {
 //     for (int j = 0; j < array.GetLength(1); j++)
