44f024a63cdf665b25f78f6beb303e62c3b4373b
Helen <e.xolodkova@inbox.ru>
2022-05-23 21:11:28 +0300

    Решение задачи №66 к семинару 9

---
+++ Lesson_9_Task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 4, N = 8;
+
+int RecSum(int n, int m)
+{
+    if(m > n)
+        return 0;
+    System.Console.Write(m + " ");
+    return RecSum(n, m+1) + m;
+}
+
+int summa = RecSum(N,M);
+System.Console.WriteLine();
+System.Console.Write($"Сумма натуральных элементов в промежутке от {M} до {N}: ");
+System.Console.WriteLine(summa);
+
+
